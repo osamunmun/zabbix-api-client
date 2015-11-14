0147385294ba62f63ce84eff9f9fd2cd61fb243f
osamunmun <osamu.takayasu@gmail.com>
2015-11-14 13:59:18 +0900

    setup.pyに定義を追加

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name="zabbix-api-client")
+long_description = open('./README.rst').read()
+
+setup(
+    name="zabbix-api-client",
+    version="0.0.1",
+    install_requires=[],
+    description='Zabbix API client library',
+    long_description=long_description,
+    url='https://github.com/osamunmun/zabbix-api-client',
+    author='Osamu Takayasu',
+    author_email='osamu.takayasu@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'zabbix_cl = zabbbix-api-client.core:main',
+        ]
+    }
+)
