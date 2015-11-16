daab1bf4a38e342caf55e51933ac6a6820110448
osamunmun <osamu.takayasu@nex.nikkei.com>
2015-11-16 12:08:26 +0900

    Add log level argument for init.

--- zabbix-api-client/client.py
+++ zabbix-api-client/client.py
@@ -12,8 +12,7 @@ class Borg(object):
 class Client(Borg):
     """Zabbix API"""
 
-    def __init__(self, host='', user='', password=''):
-
+    def __init__(self, host='', user='', password='', log_level='WARNING'):
         """ Client instance Constructor
 
         :param host: zabbix host name
@@ -23,14 +22,15 @@ class Client(Borg):
         Borg.__init__(self)
         if not hasattr(self, 'logger'):
             self.logger = logging.getLogger(__name__)
-            self.logger.setLevel(logging.INFO)
+            loglevel_obj = getattr(logging, log_level.upper())
+            self.logger.setLevel(loglevel_obj)
             ch = logging.StreamHandler()
             ch.setLevel(logging.INFO)
             self.logger.addHandler(ch)
         self.logger.info('Start initializing')
         self.host = host
         self.request_id = 1
-        if not hasattr(self, 'auth'):
+        if not hasattr(self, 'auth') or not self.auth:
             self.auth = self.request('user.login', {'user': user, 'password': password})
 
 
@@ -43,7 +43,10 @@ class Client(Borg):
                            'auth': (self.auth if hasattr(self, 'auth') else None),
                            'id': self.request_id})
         self.logger.info('URL:%s\tMETHOD:%s\tPARAM:%s', self.host, method, str(params))
-        r = requests.post(self.host, data=data, headers=headers)
-        self.logger.info('STATUS_CODE:%s', r.status_code)
-        return r.json()['result']
+        try:
+            r = requests.post(self.host, data=data, headers=headers)
+            self.logger.info('STATUS_CODE:%s', r.status_code)
+            return r.json()['result']
+        except Exception as e:
+            self.logger.error('TYPE:%s\tMESSAGE:%s', str(type(e)), e.args)
 
