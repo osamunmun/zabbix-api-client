04fb6416d25d78e6970d12a7da8c2a08548cf44f
osamunmun <osamu.takayasu@nex.nikkei.com>
2015-11-18 17:39:13 +0900

    Update maintenance resouce.

--- zabbix-api-client/client.py
+++ zabbix-api-client/client.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import logging
+import jsonschema
+import dateutil.parser
 
 
 class Borg(object):
@@ -12,26 +14,29 @@ class Borg(object):
 class Client(Borg):
     """Zabbix API"""
 
-    def __init__(self, host='', user='', password='', log_level='WARNING'):
+    def __init__(self, **kwargs):
         """ Client instance Constructor
 
         :param host: zabbix host name
         :param user: access user
         :param password: access user's password
+        :param log_level: log level
         """
         Borg.__init__(self)
-        if not hasattr(self, 'logger'):
+        if not hasattr(self, 'logger') or not self.logger:
             self.logger = logging.getLogger(__name__)
-            loglevel_obj = getattr(logging, log_level.upper())
+            log_level = 'WARNING' if not 'log_level' in kwargs else kwargs['log_level'].upper()
+            loglevel_obj = getattr(logging, log_level)
             self.logger.setLevel(loglevel_obj)
             ch = logging.StreamHandler()
             ch.setLevel(logging.INFO)
             self.logger.addHandler(ch)
         self.logger.info('Start initializing')
-        self.host = host
+        self.host = kwargs['host'] + '/zabbix/api_jsonrpc.php'
         self.request_id = 1
         if not hasattr(self, 'auth') or not self.auth:
-            self.auth = self.request('user.login', {'user': user, 'password': password})
+            self.auth = self.request('user.login', {'user': kwargs['user'],
+                                                    'password': kwargs['password']})
 
 
     def request(self, method, params):
@@ -45,8 +50,15 @@ class Client(Borg):
         self.logger.info('URL:%s\tMETHOD:%s\tPARAM:%s', self.host, method, str(params))
         try:
             r = requests.post(self.host, data=data, headers=headers)
+            if 'error' in r.json():
+                raise Exception(r.json()['error'])
             self.logger.info('STATUS_CODE:%s', r.status_code)
             return r.json()['result']
         except Exception as e:
             self.logger.error('TYPE:%s\tMESSAGE:%s', str(type(e)), e.args)
 
+    def validate(self, params, schema):
+        jsonschema.validate(params, schema)
+
+    def unixtime(self, datetime):
+        return int(dateutil.parser.parse(datetime).timestamp())
